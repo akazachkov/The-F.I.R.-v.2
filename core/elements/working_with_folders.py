2f5ce6526bf013275ce146cc7e708fd34517e091
akazachkov <akazachkov@ya.ru>
2026-01-27 17:14:34 +0300

    27.01.2026 1. Создан файл для методов, для работы с директориями и файлами.

--- core/elements/working_with_folders.py
+++ core/elements/working_with_folders.py
@@ -1,19 +1,31 @@
-# app/core/elements/open_file_or_folder.py
+# app/core/elements/working_with_folders.py
 
 import subprocess
 from pathlib import Path
 
 
-# Функция для открытия файла и директории (универсальная)
+def ensure_directory_exists(base_path: Path, subfolder: str) -> Path:
+    """
+    Создает директорию, если она не существует.
+
+    Args:
+        base_path - Путь к базовой директории.
+        subfolder - Имя новой поддиректории.
+    """
+    directory = base_path / subfolder
+    directory.mkdir(parents=True, exist_ok=True)
+    return directory
+
+
 def open_file_and_folder(path: str | Path) -> bool:
     """
     Открывает указанную директорию или файл в Проводнике Windows.
 
     Args:
-        path: Строка или Path-объект, указывающий на директорию.
+        path - Строка или Path-объект, указывающий на директорию.
 
     Returns:
-        bool: True, если команда была успешно запущена, иначе False.
+        bool - True, если команда была успешно запущена, иначе False.
     """
     try:
         # Нормализуем путь, преобразуя его в Windows-совместимый вид

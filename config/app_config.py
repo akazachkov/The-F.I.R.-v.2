a2f9c996b533c935ce9852181fd8c9818a169618
akazachkov <akazachkov@ya.ru>
2026-02-26 14:36:48 +0300

    26.02.2026 - v. 1.2.0 1. Работа с модулями-вкладками вынесена в отдельный функционал. При инициализации модуля происходит проверка типа, для создания фрейма или вкладки.

--- config/app_config.py
+++ config/app_config.py
@@ -11,7 +11,7 @@ MODULES_DIR = PROJECT_ROOT / "modules"
 CONFIG_PATHS_NAME = PROJECT_ROOT / "config" / "config_paths_name_local.toml"
 
 # Название приложения, логотип в строке заголовка
-APP_TITLE = "P.R.I.S.M. v.1.1.0"
+APP_TITLE = "P.R.I.S.M. v.1.2.0"
 # APP_LOGO = PROJECT_ROOT / "images" / "logo.png"
 
 # Геометрия приложения
